2d416b107090a921ff13e228dc0d7ece90c5c889
Odd Mathisen <odda2k@hotmail.com>
2026-08-08 00:04:39 +0200

    🔖 Bump version to 1.6.9

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 #define RB_VERSION_MAJOR 1 // NOLINT(modernize-macro-to-enum)
 #define RB_VERSION_MINOR 6 // NOLINT(modernize-macro-to-enum)
-#define RB_VERSION_PATCH 8 // NOLINT(modernize-macro-to-enum)
+#define RB_VERSION_PATCH 9 // NOLINT(modernize-macro-to-enum)
 
 #include "version_build.h"
 
